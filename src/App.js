acde873fd2c13438d492af51f7db19f197aae08c
Jeann11 <jeanneth8464@gmail.com>
2023-03-24 11:55:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import Data from './components/Data';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import axios from "axios";
 
+axios.defaults.baseURL = 'http://localhost:8081/';
 
 function App() {
-  return 
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Data></Data>}></Route>
+    </Routes>
+    </BrowserRouter> 
+  );
 }
 
 export default App;
---
+++ src/components/Data.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import DataServices from "../service/DataServices";
+
+const Data = () => {
+  const [data, SetData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      
+      const dataResponse = await DataServices();
+      SetData(dataResponse)
+    } 
+
+    fetchData();
+
+    
+     });
+
+  return (
+    <div>
+      <p>You can count on me</p>
+    </div>
+  );
+};
+
+export default Data;
