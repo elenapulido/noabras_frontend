5e65cd7f1e44f12c49bf3569f4c70487bfec298e
Jeann11 <jeanneth8464@gmail.com>
2023-03-27 22:28:14 +0200

    viewLegendsAndHome

--- src/service/GetData.jsx
+++ src/service/GetData.jsx
@@ -1,24 +1,13 @@
 import axios from "axios";
-import React from "react";
-import http from "../httpApi";
 
-export default function GetData() {
-  const [model, setModel] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(http).then((response) => {
-      setModel(response.data);
-    });
-  }, []);
-
-  if (!model) return null;
-
-  return (
-    <div>
-      <h1>{model.name}</h1>
-      <p>{model.description}</p>
-      <p>{model.location}</p>
-      <image>{model.url}</image>
-    </div>
-  );
+async function GetData() {
+  try {
+    const response = await axios.get("");
+    console.log(response.data)
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+export default GetData;
