573a9e3c5675c5fceffb1bb930773598d9358a27
Aurino04 <a_llera_rodriguez@hotmai.l.com>
2023-03-28 12:31:07 +0200

    footer and card

--- src/service/GetData.jsx
+++ src/service/GetData.jsx
@@ -10,16 +10,7 @@ async function GetData() {
   }
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 export default GetData;
-=======
-export default GetData;
->>>>>>> 628a679 (footer and card)
-=======
-export default GetData;
->>>>>>> 628a679 (footer and card)
-=======
-export default GetData;
->>>>>>> 628a679 (footer and card)
+
+
+
