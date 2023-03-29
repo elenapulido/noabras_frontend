258f1ba145fc49b92b978ee0539ffd9719d31f85
JavierBP <fjavierbresperez@yahoo.es>
2023-03-29 15:41:47 +0200

    navbar added

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Home.css";
 import BodyHome from "../../components/body/BodyHome";
+import Navbar from "../../components/navbar/Navbar"
 
 function Home() {
 
     return (
         <div>
-
+            <Navbar />
             <BodyHome />           
 
         </div>
