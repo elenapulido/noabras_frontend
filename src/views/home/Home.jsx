0dbfcd2f7ea1a5b0bcb2a4e08e32322c84de9a48
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-29 09:19:02 +0200

    changes

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -7,38 +7,10 @@ import BodyView from "../../components/body/BodyView";
 import Form from "../../components/form/Form";
 
 
-function Home({ title, text, url }) {
-  
-  return (
-    <div className="card text-center bg-dark animate__animated animate__fadeInUp">
-      <div className="overflow">
-        <Image src={cuentosyleyendas} fluid className="card-img-top" />
-      </div>
-      <div className="card-body text-light">
-        <h4 className="card-title">{title}</h4>
-        <p className="card-text text-secondary">
-          {text
-            ? text
-            : "En nuestra web encontrarás  narraciones de hechos sobrenaturales, naturales, paranormales e increíbles o una mezcla de ellos, que se han transmitido de generación en generación en forma oral o escrita y que hoy compartimos con vosotros "}
-        </p>
-        <a
-          href={url ? url : "/paranormal"}
-          target="_blank"
-          className="btn btn-outline-secondary border-0"
-          rel="noreferrer"
-        >
-          Go to {title}
-        </a>
-      </div>
-    </div>
-  );
+function Home() {
 
 
-
-
-
-
-   
+    return (
         <div>
             <BodyView />
             <Form />
@@ -46,12 +18,5 @@ function Home({ title, text, url }) {
         </div>
     )
 }
-
-Home.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string,
-  url: PropTypes.string,
-  imageSource: PropTypes.string
-};
 
 export default Home;
