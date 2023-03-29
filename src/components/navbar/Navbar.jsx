20c69699fcda59883e6b47d98f9ff7b306de6e36
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-29 15:26:13 +0200

    links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import "./Navbar.css";
 
-const Navbar = () => { 
+const Navbar = () => {
 
   return (
-    <div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link to="/"><img src={logo} alt="logo" /></Link>
 
-      <div className='main-header'>
-          <h1>NO ABRAS LA PUERTA</h1>
-      </div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-    </div>
+
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Menu
+                </a>
+                <ul className="dropdown-menu">
+                  <Link to="/leyendas"><li><p className="dropdown-item" >Experiencias paranormales</p></li></Link>
+                  <Link to="/historias"><li><p className="dropdown-item" > Tus historias</p></li></Link>
+                  <Link to="/"><li><p className="dropdown-item" >Psicofonías</p></li></Link>
+                </ul>
+              </li>
+
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+
+        </div>
+      </nav>
+    </>
   )
 }
 
