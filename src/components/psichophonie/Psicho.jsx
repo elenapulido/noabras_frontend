fd01ed3ac11c20ea93f1dd05a5f4a95e8ab6a417
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-29 18:58:27 +0200

    psicho

---
+++ src/components/psichophonie/Psicho.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Psicho() {
+  return (
+    <div>
+         <div id='playAlbum' class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <iframe src="https://open.spotify.com/embed/album/4xwx0x7k6c5VuThz5qVqmV?utm_source=generator" width="50%" height="352" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default Psicho
--- src/views/psychophonie/Psychophonie.jsx
+++ src/views/psychophonie/Psychophonie.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
+import Psicho from '../../components/psichophonie/Psicho'
 
 function Psychophonie() {
   return (
     <div>
       <Navbar />
-      <div id='playAlbum' class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <iframe src="https://open.spotify.com/embed/album/4xwx0x7k6c5VuThz5qVqmV?utm_source=generator" width="50%" height="352" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      </div>
+     <Psicho/>
 
       <Footer />
 
