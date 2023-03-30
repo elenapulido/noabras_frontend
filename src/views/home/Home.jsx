2c1d4231b184139897905ec47bafc78afabbef17
elenapulido <119506074+elenapulido@users.noreply.github.com>
2023-03-30 09:06:46 +0200

    bodyHome

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import BodyHome from "../../components/body/BodyHome";
 import Navbar from "../../components/navbar/Navbar";
-import Footer from "../../components/footer/Footer";
+import Footer from "../../components/footer/Footer"
+import BodyHome from "../../components/body/BodyHome";
+
+
 
 function Home() {
 
     return (
         <div>
             <Navbar />
-            <BodyHome />                              
+            <BodyHome />
+           
             <Footer />
 
         </div>
